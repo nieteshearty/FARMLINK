b8a910ab6c9e781ac2f32a73c1c350985e6fafc8
Hearty Nietes <nieteshearty@gmail.com>
2025-11-12 15:15:25 +0800

    git commit -a

---
+++ wasmer.php
@@ -0,0 +1,16 @@
+<?php
+require_once __DIR__ . '/api/config.php';
+header('Content-Type: application/json');
+$action = $_GET['action'] ?? '';
+if ($action === 'liveconfig') {
+    $basePath = (strpos($_SERVER['REQUEST_URI'] ?? '', '/FARMLINK/') !== false || strpos($_SERVER['HTTP_REFERER'] ?? '', '/FARMLINK/') !== false) ? '/FARMLINK' : '';
+    echo json_encode([
+        'ok' => true,
+        'basePath' => $basePath,
+        'https' => isHTTPS(),
+        'timestamp' => time()
+    ]);
+    exit;
+}
+http_response_code(404);
+echo json_encode(['ok' => false, 'error' => 'not_found']);
